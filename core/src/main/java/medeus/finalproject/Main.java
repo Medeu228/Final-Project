57e334f75d7fd131d73bdca820583bfda3c360d3
Medeu228 <iakubovmedeu@gmail.com>
2026-03-25 09:24:05 +0500

    hotfix 3

--- core/src/main/java/medeus/finalproject/Main.java
+++ core/src/main/java/medeus/finalproject/Main.java
@@ -2,25 +2,11 @@ package medeus.finalproject;
 
 import com.badlogic.gdx.Game;
 import medeus.finalproject.Screens.GameScreen;
-import medeus.finalproject.Menu.MainMenuScreenDemo;
 
 public class Main extends Game{
 
     @Override
     public void create() {
-        setScreen(new MainMenuScreenDemo(this));
-    }
-
-    @Override
-    public void render () {
-        // ОБЯЗАТЕЛЬНО: Вызываем render базового класса Game,
-        // иначе setScreen() не будет работать.
-        super.render();
-    }
-
-    @Override
-    public void dispose () {
-        super.dispose();
-        // Очистка ресурсов главного класса, если они есть
+        setScreen(new GameScreen());
     }
 }
